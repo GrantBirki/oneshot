b89aec9f3f00485ebcfcadcb23440a3cc7747c75
GrantBirki <grant.birkinbine@gmail.com>
2026-01-12 21:58:29 -0800

    Add initial project configuration and macOS support files

---
+++ Sources/AppDelegate.swift
@@ -0,0 +1,18 @@
+import Cocoa
+
+final class AppDelegate: NSObject, NSApplicationDelegate {
+    private var statusItem: NSStatusItem?
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        let item = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        let menu = NSMenu()
+        menu.addItem(NSMenuItem(title: "Quit OpenShot", action: #selector(quit), keyEquivalent: "q"))
+        item.button?.title = "OpenShot"
+        item.menu = menu
+        statusItem = item
+    }
+
+    @objc private func quit() {
+        NSApp.terminate(nil)
+    }
+}
