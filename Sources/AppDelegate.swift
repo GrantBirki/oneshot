8207d162cffdc800d03b3c6e8cc566e257b69803
GrantBirki <grant.birkinbine@gmail.com>
2026-01-12 22:06:43 -0800

    Refactor macOS application structure: update Info.plist, enhance .gitignore, and remove Main.swift

--- Sources/AppDelegate.swift
+++ Sources/AppDelegate.swift
@@ -1,9 +1,11 @@
 import Cocoa
 
+@main
 final class AppDelegate: NSObject, NSApplicationDelegate {
     private var statusItem: NSStatusItem?
 
     func applicationDidFinishLaunching(_ notification: Notification) {
+        NSApp.setActivationPolicy(.accessory)
         let item = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
         let menu = NSMenu()
         menu.addItem(NSMenuItem(title: "Quit OpenShot", action: #selector(quit), keyEquivalent: "q"))
