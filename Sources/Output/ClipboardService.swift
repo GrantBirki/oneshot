6ba6b65c731b4697cdd78587238900efc20bfe48
GrantBirki <grant.birkinbine@gmail.com>
2026-01-14 19:08:44 -0800

    Refactor ClipboardService: Update copy method to accept a custom NSPasteboard parameter and enhance functionality to support TIFF data alongside PNG.

--- Sources/Output/ClipboardService.swift
+++ Sources/Output/ClipboardService.swift
@@ -1,9 +1,17 @@
 import AppKit
 
 enum ClipboardService {
-    static func copy(pngData: Data) {
-        let pasteboard = NSPasteboard.general
+    static func copy(pngData: Data, to pasteboard: NSPasteboard = .general) {
         pasteboard.clearContents()
+        let tiffData = NSImage(data: pngData)?.tiffRepresentation
+        var types: [NSPasteboard.PasteboardType] = [.png]
+        if tiffData != nil {
+            types.append(.tiff)
+        }
+        pasteboard.declareTypes(types, owner: nil)
         pasteboard.setData(pngData, forType: .png)
+        if let tiffData = tiffData {
+            pasteboard.setData(tiffData, forType: .tiff)
+        }
     }
 }
